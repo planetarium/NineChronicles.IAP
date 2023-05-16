812676897c74bef1b86b6392d71953ed1067da3f
hyeon <hyeon@planetariumhq.com>
2023-05-16 23:02:16 +0900

    Set DB Basics
    - Setup Base declarative model
    - Setup alembic to manage database migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,16 @@
+import sys
+from importlib import import_module
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
+
+sys.path = ["", ".."] + sys.path[1:]
+from iap import models
+
+for model in models.__all__:
+    import_module(f".{model}", "iap.models")
+from iap.models.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +25,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
