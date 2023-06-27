4446b32bbd8096375ef8fc552ab94cf547277c84
hyeon <hyeon@planetariumhq.com>
2023-06-27 16:05:30 +0900

    Add detailed message to exception

--- iap/exceptions.py
+++ iap/exceptions.py
@@ -1,11 +1,16 @@
-from typing import Union
+from typing import Union, Optional
 from uuid import uuid4
 
 
 class ReceiptNotFoundException(Exception):
-    def __init__(self, receipt_uuid: Union[str, uuid4]):
+    def __init__(self, receipt_uuid: Union[str, uuid4], order_id: Optional[str]):
         super().__init__()
         self.receipt_uuid = receipt_uuid
+        self.order_id = order_id
 
     def __str__(self):
-        return f"Receipt {self.receipt_uuid} not found."
+        if self.receipt_uuid:
+            return f"Receipt {self.receipt_uuid} not found."
+        elif self.order_id:
+            return f"Receipt {self.order_id} not found."
+
