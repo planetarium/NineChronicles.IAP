d5918810a1123344640f2305b88b203ccb760ea8
hyeon <hyeon@planetariumhq.com>
2023-09-18 13:27:15 +0900

    Split form sheet and work sheet

--- worker/worker_cdk_stack.py
+++ worker/worker_cdk_stack.py
@@ -183,6 +183,7 @@ class WorkerStack(Stack):
 
         # Golden dust by NCG handler
         env["GOLDEN_DUST_REQUEST_SHEET_ID"] = config.golden_dust_request_sheet_id
+        env["GOLDEN_DUST_WORK_SHEET_ID"] = config.golden_dust_work_sheet_id
         env["FORM_SHEET"] = config.form_sheet
         gd_handler = _lambda.Function(
             self, f"{config.stage}-9c-iap-goldendust-handler-function",
