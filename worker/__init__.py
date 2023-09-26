0a8938abb02adf06b19b31665fbc89f7525d2653
hyeon <hyeon@planetariumhq.com>
2023-09-27 02:11:36 +0900

    Update CDK stack to apply new file structure

--- worker/__init__.py
+++ worker/__init__.py
@@ -1,2 +1,11 @@
 WORKER_LAMBDA_EXCLUDE = [
+    ".aws-sam",
+    "layer",
+    "tests",
+    ".gitignore",
+    "README.md",
+    "requirements.txt",
+    "sample_*.json*",
+    "template.yml*",
+    "worker_cdk_stack.py",
 ]
