a23379df7bde8465461887a89f6c643d5263facd
hyeon <hyeon@planetariumhq.com>
2023-09-27 11:54:09 +0900

    Add uuid column to SignHistory to prevent duplicated handling

--- common/models/sign.py
+++ common/models/sign.py
@@ -1,10 +1,14 @@
-from sqlalchemy import Column, Text, Integer
+import uuid
+
+from sqlalchemy import Column, Text, Integer, UUID
 
 from common.models.base import AutoIdMixin, TimeStampMixin, Base
 
 
 class SignHistory(AutoIdMixin, TimeStampMixin, Base):
     __tablename__ = "sign_history"
+    uuid = Column(UUID(as_uuid=True), nullable=False, index=True, default=uuid.uuid4,
+                  doc="Internal uuid for management")
     request_type = Column(Text, nullable=False, index=True)
     data = Column(Text, doc="Request data. Have to be valid JSON")
     nonce = Column(Integer, nullable=False, doc="dedicated nonce for this request.")
