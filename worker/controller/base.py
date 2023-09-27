dc29a379e674a070bde7eeff4395e6cf2b4c435e
hyeon <hyeon@planetariumhq.com>
2023-09-27 11:55:59 +0900

    Update controller
    - Receive SQSMessage and SignHistory to collect/process
    - Update IAPController to collect

--- worker/controller/base.py
+++ worker/controller/base.py
@@ -1,17 +1,27 @@
+from typing import List
+
+from sqlalchemy import select, desc
+
 from common._crypto import Account
 from common._graphql import GQL
-from worker.schema.sqs import SQSMessage
+from common.models.sign import SignHistory
+from schema.sqs import SQSMessage
 
 
 class BaseController:
-    def __init__(self, sess, message: SQSMessage, account: Account, gql: GQL):
+    def __init__(self, sess, account: Account, gql: GQL):
         self.sess = sess
-        self.message = message
         self.account = account
         self.gql = gql
 
-    def collect(self):
+    @property
+    def next_nonce(self) -> int:
+        next_nonce = self.gql.get_next_nonce(self.account.address)
+        prev_nonce = self.sess.scalar(select(SignHistory.nonce).order_by(desc(SignHistory.nonce)).limit(1))
+        return max(next_nonce, (-1 if prev_nonce is None else prev_nonce) + 1)
+
+    def collect(self, message: SQSMessage):
         raise NotImplementedError
 
-    def process(self):
+    def process(self, history_list: List[SignHistory]):
         raise NotImplementedError
--- worker/controller/iap.py
+++ worker/controller/iap.py
@@ -1,23 +1,40 @@
+import json
+from typing import List
+
 from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 
 from common import logger
 from common._crypto import Account
 from common._graphql import GQL
+from common.models.product import Product
 from common.models.receipt import Receipt
-from worker.controller.base import BaseController
-from worker.schema.sqs import SQSMessage
+from common.models.sign import SignHistory
+from controller.base import BaseController
+from schema.sqs import SQSMessage
 
 
 class IAPController(BaseController):
-    def __init__(self, sess, message: SQSMessage, account: Account, gql: GQL, **kwargs):
-        super().__init__(sess, message, account, gql)
+    def __init__(self, sess, account: Account, gql: GQL, **kwargs):
+        super().__init__(sess, account, gql)
 
-    def collect(self):
-        uuid_list = [x.body.get("uuid") for x in self.message.Records if x.body.get("uuid") is not None]
+    def collect(self, message: SQSMessage) -> List[SignHistory]:
+        nonce = self.next_nonce
+        history_list = []
+        uuid_list = [x.body.get("data", {}).get("uuid") for x in message.Records
+                     if x.body.get("data", {}).get("uuid") is not None]
         receipt_dict = {str(x.uuid): x for x in self.sess.scalars(select(Receipt).where(Receipt.uuid.in_(uuid_list)))}
-        for i, record in enumerate(self.message.Records):
-            if record.get("type") != "iap":
+        prev_collections = [str(x) for x in self.sess.scalars(select(SignHistory.uuid)).fetchall()]
+
+        for i, record in enumerate(message.Records):
+            if record.body["type"] != "iap":
+                logger.debug(f"{record.body['uuid']} is not type of iap. Skip.")
                 continue
+
+            if record.body["uuid"] in prev_collections:
+                logger.warning(f"{record.body['uuid']} already added to history. Skip.")
+                continue
+
             data = record.body.get("data", {})
             if not data:
                 logger.error(f"No request data found: {record.body}")
@@ -27,7 +44,61 @@ class IAPController(BaseController):
             if not receipt:
                 msg = f"{data.get('uuid')} not found in receipt history"
                 logger.error(msg)
-                return False, msg, None
+                continue
 
-    def process(self):
-        pass
+            history_list.append(SignHistory(
+                uuid=record.body["uuid"],
+                request_type=record.body["type"],
+                data=json.dumps(record.body.get("data", {})),
+                nonce=nonce
+            ))
+            nonce += 1
+
+        self.sess.add_all(history_list)
+        self.sess.commit()
+        # self.sess.refresh_all(history_list)
+        logger.info(f"{len(history_list)} IAP requests collected. Next nonce is {nonce}")
+        return history_list
+
+    def process(self, history_list: List[SignHistory]):
+        for history in history_list:
+            if history.plain_value is not None:
+                logger.info(f"History #{history.id} has already been processed. Please check plain value and nonce.")
+                continue
+
+            request_data = json.loads(history.data)
+            if not request_data:
+                history.plain_value = ""
+                history.tx_id = ""
+                continue
+
+            product = self.sess.scalar(
+                select(Product)
+                .options(joinedload(Product.fav_list)).options(joinedload(Product.fungible_item_list))
+                .where(Product.id == request_data.get("product_id"))
+            )
+
+            fav_data = [{
+                "balanceAddr": request_data.get("agent_addr"),
+                "fungibleAssetValue": {
+                    "currency": x.currency.name,
+                    "majorUnit": x.amount,
+                    "minorUnit": 0
+                }
+            } for x in product.fav_list]
+
+            item_data = [{
+                "fungibleId": x.fungible_item_id,
+                "count": x.amount
+            } for x in product.fungible_item_list]
+
+            plain_value = self.gql.create_action(
+                "unload_from_garage", pubkey=self.account.pubkey, nonce=history.nonce, tx=False,
+                fav_data=fav_data, avatar_addr=request_data.get("avatar_addr"), item_data=item_data
+            )
+            unsigned_tx = self.gql.create_unsigned_tx(plain_value, pubkey=self.account.pubkey, nonce=history.nonce)
+            signature = self.account.sign_tx(unsigned_tx)
+            signed_tx = self.gql.sign(unsigned_tx, signature)
+            success, msg, tx_id = self.gql.stage(signed_tx)
+            history.plain_value = plain_value.hex()
+            history.tx_id = tx_id
