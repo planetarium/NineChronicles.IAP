94f88026395fa6adf8a2cc9b932350a5a0a8bf2d
hyeon <hyeon@planetariumhq.com>
2023-10-12 16:46:03 +0900

    Update apple receipt data handling

--- iap/validator/common.py
+++ iap/validator/common.py
@@ -20,8 +20,10 @@ def get_order_data(receipt_data: ReceiptSchema) -> Tuple[str, Union[str, int], d
         product_id = receipt_data.order.get("productId")
         purchased_at = datetime.fromtimestamp(receipt_data.order.get("purchaseTime") // 1000)  # Remove millisecond
     elif receipt_data.store in (Store.APPLE, Store.APPLE_TEST):
-        order_id = receipt_data.data.get("transactionId")
-        product_id = receipt_data.data.get("productId")
+        order_id = receipt_data.data.get("TransactionID")
+        # product_id = receipt_data.data.get("productId")
+        # Apple does not provide productId in receipt data
+        product_id = 0
         purchased_at = datetime.utcnow()
     else:
         raise ValueError(f"{receipt_data.store.name} is unsupported store.")
