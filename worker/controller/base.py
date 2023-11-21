d4896b3ce34556b386905148dfe24c46944ab217
hyeon <hyeon@planetariumhq.com>
2023-11-22 04:15:34 +0900

    Base controller has planet info

--- worker/controller/base.py
+++ worker/controller/base.py
@@ -1,10 +1,14 @@
+import json
+import os
 from typing import List
 
+import requests
 from sqlalchemy import select, desc
 
 from common._crypto import Account
 from common._graphql import GQL
 from common.models.sign import SignHistory
+from common.utils.receipt import PlanetID
 from schema.sqs import SQSMessage
 
 
@@ -13,6 +17,22 @@ class BaseController:
         self.sess = sess
         self.account = account
         self.gql = gql
+        self.main_planet = PlanetID.ODIN if os.environ.get("STAGE") == "mainnet" else PlanetID.ODIN_INTERNAL
+        self.planet_dict = {}
+        self.gql_url = ""
+        try:
+            resp = requests.get(os.environ.get("PLANET_URL"))
+            data = resp.json()
+            for planet in data:
+                if PlanetID(bytes(planet["id"], "utf-8")) == self.main_planet:
+                    self.gql_url = planet["rpcEndpoints"]["headless.gql"][0]
+                    self.gql.reset(self.gql_url)
+                    self.planet_dict = {
+                        PlanetID(bytes(k, "utf-8")): v for k, v in planet["bridges"].items()
+                    }
+        except:
+            # Fail over
+            self.planet_dict = json.loads(os.environ.get("BRIDGE_DATA", "{}"))
 
     @property
     def next_nonce(self) -> int:
