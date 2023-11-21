ec60c2d032e24618295b35ee4ee215c34198c7f0
hyeon <hyeon@planetariumhq.com>
2023-11-22 04:19:52 +0900

    Add planet_id to sign history

---
+++ common/alembic/versions/bdb01e673e04_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: bdb01e673e04
+Revises: 32e8805e8364, 8c0698de32b6
+Create Date: 2023-11-22 04:17:31.509452
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bdb01e673e04'
+down_revision = ('32e8805e8364', '8c0698de32b6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- common/models/sign.py
+++ common/models/sign.py
@@ -1,8 +1,9 @@
 import uuid
 
-from sqlalchemy import Column, Text, Integer, UUID
+from sqlalchemy import Column, Text, Integer, UUID, LargeBinary
 
 from common.models.base import AutoIdMixin, TimeStampMixin, Base
+from common.utils.receipt import PlanetID
 
 
 class SignHistory(AutoIdMixin, TimeStampMixin, Base):
@@ -11,6 +12,8 @@ class SignHistory(AutoIdMixin, TimeStampMixin, Base):
                   doc="Internal uuid for management")
     request_type = Column(Text, nullable=False, index=True)
     data = Column(Text, doc="Request data. Have to be valid JSON")
+    planet_id = Column(LargeBinary(length=12), nullable=False, default=PlanetID.ODIN.value,
+                       doc="An identifier of planets")
     nonce = Column(Integer, nullable=False, doc="dedicated nonce for this request.")
     plain_value = Column(Text, doc="Hex. encoded plain value of transaction.")
     tx_id = Column(Text, doc="Created Tx. Hash")
