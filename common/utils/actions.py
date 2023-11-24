1cd311dd10eebe712cfd6fbcef435bf0ff1a7f0d
hyeon <hyeon@planetariumhq.com>
2023-11-24 19:37:22 +0900

    Address inside message must not have '0x'

--- common/utils/actions.py
+++ common/utils/actions.py
@@ -14,7 +14,7 @@ def create_unload_my_garages_action_plain_value(id: str, fav_data: List, avatar_
                 bytes.fromhex(avatar_addr),
                 [
                     [
-                        bytes.fromhex(x['balanceAddr']),
+                        bytes.fromhex(x['balanceAddr'][2:] if x["balanceAddr"].startswith("0x") else x["balanceAddr"]),
                         FungibleAsset.to_fungible_asset(x['value']['currencyTicker'], x['value']['value'])
                     ]
                     for x in fav_data
