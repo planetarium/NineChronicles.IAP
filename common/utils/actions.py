414e469286273ecddf8de77ffc4fc38a0219a792
Suho Lee <iam@suho.io>
2023-11-24 15:35:18 +0900

    refactor: change create action type

--- common/utils/actions.py
+++ common/utils/actions.py
@@ -1,15 +1,12 @@
-import bencodex
-from bencodex import BValue
-
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
 
 from common.lib9c.fungible_asset import FungibleAsset
 
 
-def create_unload_my_garages_action_plain_value(id: str, fav_data: List, avatar_addr: str, item_data: List, memo: Optional[str]) -> BValue:
+def create_unload_my_garages_action_plain_value(id: str, fav_data: List, avatar_addr: str, item_data: List, memo: Optional[str]) -> Dict[str, Any]:
     if avatar_addr.startswith("0x"):
         avatar_addr = avatar_addr[2:]
-    return bencodex.dumps({
+    return {
         'type_id': 'unload_from_my_garages',
         'values': {
             'id': bytes.fromhex(id),
@@ -32,4 +29,4 @@ def create_unload_my_garages_action_plain_value(id: str, fav_data: List, avatar_
                 memo if memo else None,
             ]
         }
-    })
+    }
