389d2cfdd8825e32fd185d19ba8a727dfb3a6b1c
hyeon <hyeon@planetariumhq.com>
2023-11-29 19:14:21 +0900

    Apply decimalPlacecs when creating Tx

--- common/lib9c/fungible_asset.py
+++ common/lib9c/fungible_asset.py
@@ -6,11 +6,11 @@ from common.lib9c.currency import Currency
 
 
 class FungibleAsset():
-    
+
     @staticmethod
-    def to_fungible_asset(ticker: str, amount: int) -> List[Union[Dict[str, Union[str, int, None]], int]]:
-        return [Currency.to_currency(ticker), amount]
-    
+    def to_fungible_asset(ticker: str, amount: int, decimalPlaces: int) -> List[Union[Dict[str, Union[str, int, None]], int]]:
+        return [Currency.to_currency(ticker), amount * 10 ** decimalPlaces]
+
     @staticmethod
     def serialize(fungible_asset: List[Union[Dict[str, Union[str, int, None]], int]]) -> bytes:
         return bencodex.dumps([Currency.serialize(fungible_asset[0]), fungible_asset[1]])
