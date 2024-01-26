3b7bc1b7de70952ca198ce765c61160f8ad015e6
hyeon <hyeon@planetariumhq.com>
2024-01-26 13:25:16 +0900

    Use decimal for calulate and cast into integer

--- common/lib9c/models/fungible_asset_value.py
+++ common/lib9c/models/fungible_asset_value.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from decimal import Decimal
 from typing import Dict, List, Optional, Any
 
 import bencodex
@@ -8,7 +9,7 @@ from common.lib9c.models.currency import Currency
 
 
 class FungibleAssetValue:
-    def __init__(self, currency: Currency, amount: float):
+    def __init__(self, currency: Currency, amount: Decimal):
         self.currency = currency
         self.amount = amount
 
@@ -19,7 +20,7 @@ class FungibleAssetValue:
     def from_raw_data(
             cls,
             ticker: str, decimal_places: int, minters: Optional[List[str]] = None, total_supply_trackable: bool = False,
-            amount: float = 0
+            amount: Decimal = Decimal('0')
     ):
         return cls(
             Currency(ticker, decimal_places, minters, total_supply_trackable),
@@ -27,8 +28,8 @@ class FungibleAssetValue:
         )
 
     @property
-    def plain_value(self) -> List[Dict[str, Any] | float]:
-        return [self.currency.plain_value, self.amount * max(1, 10 ** self.currency.decimal_places)]
+    def plain_value(self) -> List[Dict[str, Any] | int]:
+        return [self.currency.plain_value, int(self.amount * max(1, 10 ** self.currency.decimal_places))]
 
     @property
     def serialized_plain_value(self) -> bytes:
