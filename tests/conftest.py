7d84960af5efee97451cdfc2f7042c52b71111b2
hyeon <hyeon@planetariumhq.com>
2024-03-28 19:28:44 +0900

    Setup and teardown test DB

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,27 @@
+import os
+
+import alembic
 import pytest
+from alembic.config import Config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 
-@pytest.fixture
-def session():
-    DB_URI = ""
-    engine = create_engine(DB_URI)
+@pytest.fixture(scope="session", autouse=True)
+def setup_alembic():
+    config = Config("common/alembic.ini")
+    config.set_main_option("script_location", "common/alembic")
+    config.set_main_option("sqlalchemy.url", os.environ.get('DB_URI'))
+    try:
+        alembic.command.upgrade(config, "head")
+        yield
+    finally:
+        alembic.command.downgrade(config, "base")
+
+
+@pytest.fixture(scope="function")
+def session(setup_alembic):
+    engine = create_engine(os.environ.get("DB_URI"))
     sess = scoped_session(sessionmaker(bind=engine))
     try:
         yield sess
