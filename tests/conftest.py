f7e028969c434c88629d55d9bd8b2aad03f035b7
hyeon <hyeon@planetariumhq.com>
2024-03-28 19:29:32 +0900

    Add helpers for test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
 import os
+from contextlib import contextmanager
+from typing import List
 
 import alembic
 import pytest
 from alembic.config import Config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+
+from common.models.product import Product
+from common.models.receipt import Receipt
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -28,3 +33,33 @@ def session(setup_alembic):
     finally:
         sess.rollback()
         sess.close()
+
+
+@contextmanager
+def add_product(sess, product_list: List[Product]):
+    try:
+        for product in product_list:
+            sess.add(product)
+        sess.commit()
+        for p in product_list:
+            sess.refresh(p)
+        yield product_list
+    finally:
+        for product in product_list:
+            sess.delete(product)
+        sess.commit()
+
+
+@contextmanager
+def add_receipt(sess, receipt_list: List[Receipt]):
+    try:
+        for receipt in receipt_list:
+            sess.add(receipt)
+        sess.commit()
+        for r in receipt_list:
+            sess.refresh(r)
+        yield receipt_list
+    finally:
+        for receipt in receipt_list:
+            sess.delete(receipt)
+        sess.commit()
