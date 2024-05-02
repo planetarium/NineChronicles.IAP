a3279145effcbb073fc0b20d0ba8365f90d96e0b
hyeon <hyeon@planetariumhq.com>
2024-05-02 11:20:10 +0900

    Return CDN host by package

--- iap/api/l10n.py
+++ iap/api/l10n.py
@@ -1,7 +1,10 @@
 import os
 
 from fastapi import APIRouter
+from starlette.responses import JSONResponse
+from starlette.status import HTTP_404_NOT_FOUND
 
+from common import logger
 from iap.schemas.l10n import L10NSchema
 
 router = APIRouter(
@@ -9,11 +12,22 @@ router = APIRouter(
     tags=["L10N"],
 )
 
+CDN_HOST_DICT = {
+    "com.planetariumlabs.ninechroniclesmobile": os.environ.get("CDN_HOST", "http://localhost"),
+    "com.planetariumlabs.ninek": os.environ.get("CDN_HOST_K", "http://localhost"),
+}
+
 
 @router.get("", response_model=L10NSchema)
-def l10n_list():
+def l10n_list(package_name: str):
+    host = CDN_HOST_DICT.get(package_name)
+    if not host:
+        msg = f"No CDN host for package {package_name}"
+        logger.error(msg)
+        return JSONResponse(status_code=HTTP_404_NOT_FOUND, content=msg)
+
     return L10NSchema(
-        host=os.environ.get("CDN_HOST", "http://localhost"),
+        host=CDN_HOST_DICT.get(package_name),
         category="shop/l10n/category.csv",
         product="shop/l10n/product.csv"
     )
