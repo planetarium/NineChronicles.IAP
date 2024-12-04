8f61642f0c398a98422f02908c1407be99e535d0
hyeon <hyeon@planetariumhq.com>
2024-12-04 11:52:23 +0900

    Make planet_id nullable

--- common/models/mileage.py
+++ common/models/mileage.py
@@ -7,7 +7,7 @@ from common.utils.receipt import PlanetID
 class Mileage(AutoIdMixin, TimeStampMixin, Base):
     __tablename__ = "mileage"
     agent_addr = Column(Text, nullable=False)
-    planet_id = Column(LargeBinary(length=12), nullable=False, default=PlanetID.ODIN.value,
+    planet_id = Column(LargeBinary(length=12), nullable=True, default=PlanetID.ODIN.value,
                        doc="An identifier of planets")
     mileage = Column(Integer, nullable=False, default=0, doc="Current mileage balance")
 
