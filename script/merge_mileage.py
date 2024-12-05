ce0f4354d6cc285139c493734c79bb66e7dddac1
hyeon <hyeon@planetariumhq.com>
2024-12-05 14:02:52 +0900

    Update mileage merge scripts
    - This must be executed with alembic update step by step
    - Create history table
    - Move mileage to history
    - Update schema again: remove planet_id in Mileage
    - Merge from history to mileage

--- script/merge_mileage.py
+++ script/merge_mileage.py
@@ -3,7 +3,7 @@ import sys
 from sqlalchemy import create_engine, select, delete
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from common.models.mileage import Mileage
+from common.models.mileage import Mileage, MileageHistory
 from common.utils.receipt import PlanetID
 
 
@@ -26,20 +26,20 @@ def merge(db_uri: str, dry_run: bool = True):
     print("Delete old merged data")
     sess.execute(delete(Mileage).where(Mileage.planet_id.is_(None)))
 
-    all_mileage_list = sess.scalars(select(Mileage)).fetchall()
-    print(f"Total {len(all_mileage_list)} mileages found.")
+    history_list = sess.scalars(select(MileageHistory)).fetchall()
+    print(f"Total {len(history_list)} histories found to merge.")
 
     merged = {}
-    print(f"{len(all_mileage_list)} mileages to merge")
+    print(f"{len(history_list)} mileages to merge")
 
-    for i, m in enumerate(all_mileage_list):
+    for i, m in enumerate(history_list):
         target = merged.get(m.agent_addr)
         if not target:
             merged[m.agent_addr] = target = Mileage(agent_addr=m.agent_addr, planet_id=None, mileage=0)
         prev = target.mileage
         target.mileage += m.mileage
         print(
-            f"[{i + 1:4d}/{len(all_mileage_list):4d}] [{PlanetID(m.planet_id).name}] "
+            f"[{i + 1:4d}/{len(history_list):4d}] [{PlanetID(m.planet_id).name}] "
             f"{target.agent_addr}: {prev} + {m.mileage} = {target.mileage}"
         )
 
