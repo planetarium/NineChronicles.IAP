b8ffb8df87de75fce560b243b31b1428c9e4cef0
hyeon <hyeon@planetariumhq.com>
2024-12-05 14:01:47 +0900

    Create mileage history table
    - Create history table to save old data
    - Remove planet_id from mileage table
    - This must be executed one by one with migration scripts

--- common/models/mileage.py
+++ common/models/mileage.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Text, Column, LargeBinary, Integer, Index, UniqueConstraint
+from sqlalchemy import Text, Column, LargeBinary, Integer, Index, UniqueConstraint, DateTime, func
 
 from common.models.base import AutoIdMixin, TimeStampMixin, Base
 from common.utils.receipt import PlanetID
@@ -7,11 +7,20 @@ from common.utils.receipt import PlanetID
 class Mileage(AutoIdMixin, TimeStampMixin, Base):
     __tablename__ = "mileage"
     agent_addr = Column(Text, nullable=False)
-    planet_id = Column(LargeBinary(length=12), nullable=True, default=PlanetID.ODIN.value,
-                       doc="An identifier of planets")
     mileage = Column(Integer, nullable=False, default=0, doc="Current mileage balance")
 
     __table_args__ = (
-        Index("ix_mileage_agent_planet", planet_id, agent_addr),
-        UniqueConstraint("planet_id", "agent_addr", name="unique_planet_agent"),
+        Index("ix_mileage_agent_planet", agent_addr),
+        UniqueConstraint("agent_addr", name="unique_agent"),
     )
+
+
+class MileageHistory(Base):
+    __tablename__ = "mileage_history"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    agent_addr = Column(Text, nullable=False)
+    planet_id = Column(LargeBinary(length=12), nullable=True, default=PlanetID.ODIN.value,
+                       doc="An identifier of planets")
+    mileage = Column(Integer, nullable=False, default=0, doc="Current mileage balance")
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
