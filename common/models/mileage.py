b09bf3c25eb222f20388a2efe9d2854c27d5dc3c
hyeon <hyeon@planetariumhq.com>
2024-12-10 14:04:11 +0900

    Update model and migrations
    - Remain deleted columns to run move/merge script
    - Delete all prev. merged data when downgrade model

--- common/models/mileage.py
+++ common/models/mileage.py
@@ -7,6 +7,8 @@ from common.utils.receipt import PlanetID
 class Mileage(AutoIdMixin, TimeStampMixin, Base):
     __tablename__ = "mileage"
     agent_addr = Column(Text, nullable=False)
+    # planet_id = Column(LargeBinary(length=12), nullable=True, default=PlanetID.ODIN.value,
+    #                    doc="An identifier of planets")
     mileage = Column(Integer, nullable=False, default=0, doc="Current mileage balance")
 
     __table_args__ = (
