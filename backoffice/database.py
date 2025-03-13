4376b205fee2bb6e8211aa674728586c63c6ec4d
Atralupus <me@atralupus.com>
2025-03-13 16:07:34 +0900

    Revert "temp"
    This reverts commit 939e0d5766357fd9e1541f808ed0314988eb729b.

--- backoffice/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://local_test:password@localhost/iap"  # PostgreSQL로 변경 가능
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
